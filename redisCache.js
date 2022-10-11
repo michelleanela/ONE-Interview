dc71da624903143508090b4b6d3220e9752bbcec
Adam Corrow <adam.corrow@one.app>
2022-10-11 12:04:32 -0400

    Initial js code

---
+++ redisCache.js
@@ -0,0 +1,15 @@
+module.exports = class RedisCache {
+    // TODO: Implement.
+
+    async get(key, opts) {
+        throw new Error('TODO: Implement');
+    }
+
+    async set(key, value, opts) {
+        throw new Error('TODO: Implement');
+    }
+
+    async clear(key) {
+        throw new Error('TODO: Implement');
+    }
+};
